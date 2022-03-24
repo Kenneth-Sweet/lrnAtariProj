5d5a047227b97c01bd71096efb41638186a7971b
Kenneth-Sweet <milandalosur@gmail.com>
2022-03-23 20:48:56 -0400

    Initial Commit of project

---
+++ cleanmem.asm
@@ -0,0 +1,13 @@
+    processor 6502
+
+    seg code
+    org $F000       ; Define the code origin at $F000
+
+Start:
+    sei             ; Disable interrupts
+    cld             ; Disable the BCD decimal math mode
+    ldx #$FF        ; Loads the X registere with #$FF
+    txs             ; Transfer the X register to the (S)tack pointer register
+
+; Clear the Page Zero Region ($00 to $FF)
+; Meaning the entire RAM and also TIA registers.
