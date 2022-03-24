27fc7ad401e98c269a645f30c5bd9f74806d642a
Kenneth-Sweet <milandalosur@gmail.com>
2022-03-24 16:56:44 -0400

    Code formatting and wrapping up cleanmem function

--- cleanmem.asm
+++ cleanmem.asm
@@ -2,12 +2,24 @@
 
     seg code
     org $F000       ; Define the code origin at $F000
-
 Start:
     sei             ; Disable interrupts
     cld             ; Disable the BCD decimal math mode
     ldx #$FF        ; Loads the X registere with #$FF
     txs             ; Transfer the X register to the (S)tack pointer register
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Page Zero Region ($00 to $FF)
 ; Meaning the entire RAM and also TIA registers. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #0          ; A = 0
+    ldx #$FF        ; X = #$FF
+MemLoop: 
+    sta $0,X        ; Store the value of A inside memory address $0 + X
+    dex             ; X--
+    bne MemLoop     ; Loop until X is equal to zero (z-flag is set)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC       ; $FFFC is bottom of rom cartidge
+    .word Start     ; Reset Vector at $FFFC (Where the program starts)
+    .word Start     ; inserting twice to Interrupt vector at $FFFE(unused in the vcs)
